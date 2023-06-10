5a1f00bd12957596e129610a896258322dc1dfb9
Devajit <patar1447@gmail.com>
2023-06-10 21:10:14 +0530

    UserModel Class Update

---
+++ app/src/main/java/com/dev/android/meetme/model/UserDataModel.kt
@@ -0,0 +1,13 @@
+package com.dev.android.meetme.model
+
+data class UserDataModel(
+    var name:String?="",
+    var email:String?="",
+    var city:String?="",
+    var gender:String?="",
+    var relationship:String?="",
+    var star:String?="",
+    var image:String?="",
+    var age:String?="",
+    var status:String?=""
+)
